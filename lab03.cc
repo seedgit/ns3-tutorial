6fcf85ac6ae9c2882abf3e0d4e6b8110f880dfac
Chayanon <chayanon.s@gmail.com>
2019-07-31 12:02:32 +0700

    Update lab03

--- lab03.cc
+++ lab03.cc
@@ -22,47 +22,47 @@ void ViewPosition(Ptr<Node> node) {
 int main (int argc, char *argv[])
 {
   NodeContainer nodes;
-  nodes.Create (2);
+  nodes.Create (10);
   
   MobilityHelper mobility; 
-  /*
-   
+  int mType = 0;
+  if(mType == 0) {
+    mobility.SetPositionAllocator("ns3::RandomRectanglePositionAllocator",
+      "X", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=100.0]"),
+      "Y", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=100.0]")
+    );
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.Install (nodes);
+  } else if (mType == 1) {
+    mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+                                  "MinX", DoubleValue (0.0),
+                                  "MinY", DoubleValue (0.0),
+                                  "DeltaX", DoubleValue (10),
+                                  "DeltaY", DoubleValue (10),
+                                  "GridWidth", UintegerValue (5),//Number of node in row
+                                  "LayoutType", StringValue ("RowFirst"));
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.Install (nodes);
+  } else if (mType == 2) {
+    ObjectFactory pos;
+    pos.SetTypeId ("ns3::RandomRectanglePositionAllocator");
+    pos.Set ("X", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=1000.0]"));
+    pos.Set ("Y", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=1000.0]"));
+    Ptr<PositionAllocator> taPositionAlloc = pos.Create ()->GetObject<PositionAllocator> ();
 
-  mobility.SetPositionAllocator("ns3::RandomRectanglePositionAllocator",
-    "X", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=20.0]"),
-    "Y", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=20.0]")
-  );
-  mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-  mobility.Install (nodes);
+    mobility.SetMobilityModel ("ns3::RandomWaypointMobilityModel",
+                                    "Speed", StringValue ("ns3::UniformRandomVariable[Min=10.3|Max=40.7]"),
+                                    "Pause", StringValue ("ns3::ConstantRandomVariable[Constant=0.01]"),
+                                    "PositionAllocator", PointerValue (taPositionAlloc));
 
-
-  ObjectFactory pos;
-  pos.SetTypeId ("ns3::RandomRectanglePositionAllocator");
-  pos.Set ("X", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=1000.0]"));
-  pos.Set ("Y", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=1000.0]"));
-  Ptr<PositionAllocator> taPositionAlloc = pos.Create ()->GetObject<PositionAllocator> ();
-
-  mobility.SetMobilityModel ("ns3::RandomWaypointMobilityModel",
-                                  "Speed", StringValue ("ns3::UniformRandomVariable[Min=10.3|Max=40.7]"),
-                                  "Pause", StringValue ("ns3::ConstantRandomVariable[Constant=0.01]"),
-                                  "PositionAllocator", PointerValue (taPositionAlloc));
-
-  mobility.Install (nodes);
-  */
-
-  mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
-                                 "MinX", DoubleValue (0.0),
-                                 "MinY", DoubleValue (0.0),
-                                 "DeltaX", DoubleValue (10),
-                                 "DeltaY", DoubleValue (10),
-                                 "GridWidth", UintegerValue (5),//Number of node in row
-                                 "LayoutType", StringValue ("RowFirst"));
-  mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-  
-  mobility.Install (nodes);
-
-
-  Simulator::Stop(Seconds (100.0));
+    mobility.Install (nodes);
+  }
+  for(uint32_t i=0; i < nodes.GetN(); i++){
+    Ptr<MobilityModel> m = nodes.Get(i)->GetObject<MobilityModel> ();
+    Vector pos = m->GetPosition ();
+    std::cout << "node: " << i << "(" << pos.x << "," << pos.y << ")" << std::endl;
+  }
+  //Simulator::Stop(Seconds (0.0));
   Simulator::Run ();
   Simulator::Destroy ();
 }
